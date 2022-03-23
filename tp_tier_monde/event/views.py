3ba4afab1270e704ae64222802177f22b342fb1a
Johan Valero <valero_johan@hotmail.fr>
2022-03-23 11:34:08 +0100

    add files index

--- tp_tier_monde/event/views.py
+++ tp_tier_monde/event/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Events
 
 # Create your views here.
 def index(request):
-    return HttpResponse ('Bienvenue')
+    dernier_event = Events.objects.order_by('id')
+    context = {'event_list': dernier_event}
+    return render(request, 'event/index.html', context)
--- tp_tier_monde/tp_tier_monde/urls.py
+++ tp_tier_monde/tp_tier_monde/urls.py
@@ -21,6 +21,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('event/', include ('event.urls')),
-
-
 ]
