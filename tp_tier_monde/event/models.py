5914e5fb74acaaa9cf8d5f5d25b01e359c9ee1cf
cannappin <87528956+cannappin@users.noreply.github.com>
2022-03-23 14:54:49 +0100

    str add

--- tp_tier_monde/event/models.py
+++ tp_tier_monde/event/models.py
@@ -12,20 +12,37 @@ class Users(models.Model):
     pwd_user = models.CharField(max_length=50)
     avatar_user = models.ImageField(upload_to='avatar', blank=True)
 
+    def __str__(self):
+        affiche = f"{self.name_user} {self.firstname_user}"
+        return affiche
+
 
 class EventType(models.Model):
     name_type = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name_type
+
 class HallCategory(models.Model):
     hall_category = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.hall_category
+
 class Material(models.Model):
     category_material = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_material
 
 class Artist(models.Model):
     name_artist = models.CharField(max_length=50)
     skill_artist = models.CharField(max_length=50)
     pics_artist = models.ImageField(upload_to='picsArtist', blank=True)
+
+    def __str__(self):
+        affiche = f"{self.name_artist} {self.skill_artist}"
+        return affiche
 
 
 class Groups(models.Model):
@@ -34,15 +51,23 @@ class Groups(models.Model):
     artist = models.ManyToManyField(Artist)
     pics_group = models.ImageField(upload_to='picsArtist', blank=True)
 
+    def __str__(self):
+        affiche = f"{self.name_group} {self.type_group}"
+        return affiche
+
 
 class Events(models.Model):
     name_event = models.CharField(max_length=50)
     av_ticket = models.IntegerField(NumberInput, null=True)
-    begin_date = models.DateTimeField("Date de début d'event")
+    begin_date = models.DateTimeField("Date de dÃ©but d'event")
     end_date = models.DateTimeField("Date de fin d'event")
     type = models.ForeignKey(EventType, on_delete=models.CASCADE)
     group = models.ManyToManyField(Groups,blank=True)
     user = models.ManyToManyField(Users,blank=True)
+
+    def __str__(self):
+        affiche = f" {self.name_event}  {self.av_ticket}"
+        return affiche 
 
 class Halls(models.Model):
     name_hall = models.CharField(max_length=50)
@@ -50,3 +75,7 @@ class Halls(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
     hall_category = models.ManyToManyField(HallCategory,blank=True)
+
+    def __str__(self):
+        affiche =f" {self.name_hall} {self.size_hall} {self.hall_category}"
+        return affiche
