0e8d2c2c2294d8dd01d3d5c1b19315311f08c355
Johan Valero <valero_johan@hotmail.fr>
2022-03-23 14:04:29 +0100

    add images avatar and artist pictures

--- tp_tier_monde/event/models.py
+++ tp_tier_monde/event/models.py
@@ -10,6 +10,8 @@ class Users(models.Model):
     firstname_user = models.CharField(max_length=50)
     mail_user = models.CharField(max_length=50)
     pwd_user = models.CharField(max_length=50)
+    avatar_user = models.ImageField(upload_to='avatar', blank=True)
+
 
 class EventType(models.Model):
     name_type = models.CharField(max_length=50)
@@ -23,15 +25,19 @@ class Material(models.Model):
 class Artist(models.Model):
     name_artist = models.CharField(max_length=50)
     skill_artist = models.CharField(max_length=50)
+    pics_artist = models.ImageField(upload_to='picsArtist', blank=True)
+
 
 class Groups(models.Model):
     name_group = models.CharField(max_length=50)
     type_group = models.CharField(max_length=50)
     artist = models.ManyToManyField(Artist)
+    pics_group = models.ImageField(upload_to='picsArtist', blank=True)
+
 
 class Events(models.Model):
     name_event = models.CharField(max_length=50)
-    av_ticket = models.IntegerField(NumberInput)
+    av_ticket = models.IntegerField(NumberInput, null=True)
     begin_date = models.DateTimeField("Date de début d'event")
     end_date = models.DateTimeField("Date de fin d'event")
     type = models.ForeignKey(EventType, on_delete=models.CASCADE)
@@ -40,7 +46,7 @@ class Events(models.Model):
 
 class Halls(models.Model):
     name_hall = models.CharField(max_length=50)
-    size_hall = models.IntegerField(NumberInput)
+    size_hall = models.IntegerField(NumberInput, null=True)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
     hall_category = models.ManyToManyField(HallCategory,blank=True)
