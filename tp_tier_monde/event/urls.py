d3b504603509311d159ec1ca4bff941b873fad10
cannappin <87528956+cannappin@users.noreply.github.com>
2022-03-23 12:33:58 +0100

    fonctionne

--- tp_tier_monde/event/urls.py
+++ tp_tier_monde/event/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
    path('registered/', views.registered, name='registered'),
    path('welcome/', views.welcome, name='welcome'),
    path('<int:event_id>/liste/', views.liste, name='liste'),
+   path('login/', views.my_login, name='login'),
+   path('logout/', views.my_logout, name='logout'),
+   
 ]
