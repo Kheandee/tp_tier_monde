1a1182a54bf3fee33acc2c0c7ffac857e5da3b6b
AceTBan <esteban_david@ymail.com>
2022-03-23 12:00:27 +0100

    le BON def __str__ !!

--- tp_tier_monde/event/models.py
+++ tp_tier_monde/event/models.py
@@ -11,23 +11,43 @@ class Users(models.Model):
     mail_user = models.CharField(max_length=50)
     pwd_user = models.CharField(max_length=50)
 
+    def __str__(self):
+        affiche = f'{name_user},{firstname_user}'
+        return affiche
+
 class EventType(models.Model):
     name_type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name_type
 
 class HallCategory(models.Model):
     hall_category = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.hall_category
+
 class Material(models.Model):
     category_material = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category_material
 
 class Artist(models.Model):
     name_artist = models.CharField(max_length=50)
     skill_artist = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name_artist
+
 class Groups(models.Model):
     name_group = models.CharField(max_length=50)
     type_group = models.CharField(max_length=50)
     artist = models.ManyToManyField(Artist)
+
+    def __str__(self):
+        affiche = f'{name_group},{type_group}'
+        return affiche
 
 class Events(models.Model):
     name_event = models.CharField(max_length=50)
@@ -38,9 +58,17 @@ class Events(models.Model):
     group = models.ManyToManyField(Groups,blank=True)
     user = models.ManyToManyField(Users,blank=True)
 
+    def __str__(self):
+        affiche = f'{name_event},{av_ticket},{begin_date},{end_date}'
+        return affiche
+
 class Halls(models.Model):
     name_hall = models.CharField(max_length=50)
     size_hall = models.IntegerField(NumberInput)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
     hall_category = models.ManyToManyField(HallCategory,blank=True)
+
+    def __str__(self):
+        affiche = f'{name_hall},{size_hall},{hall_category}'
+        return affiche
