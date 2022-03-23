7a2fcdaf4dfb029f3c9e9dfdc6120e96cae341cd
AceTBan <esteban_david@ymail.com>
2022-03-23 09:23:41 +0100

    migrate check

--- tp_tier_monde/tp_tier_monde/urls.py
+++ tp_tier_monde/tp_tier_monde/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('event/', include ('event.urls')),
+
 
 ]
